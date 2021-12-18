77e6d67b97618982ec9be3cc3fb80b1aaa317475
utamori <mori_yuta@retail-ai.jp>
2021-12-18 13:08:40 +0900

    eslint prettier first settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:all",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 13,
+    project: "./tsconfig.eslint.json",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "import", "jsx-a11y", "react", "react-hooks"],
+  root: true,
+  rules: {},
 };
--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 - ESLint
 - StyleLint
 - Prettier
+
+
+- eslint-plugin-react
+- eslint-plugin-react-hooks
+- @typescript-eslint/eslint-plugin
+- eslint-plugin-import
+- eslint-plugin-jsx-a11y
+- @typescript-eslint/parser”
+
+
+prettier 導入
+
+npm install prettier eslint-config-prettier --save-dev
+
+衝突するルールがないか確認
+npx eslint-config-prettier 'src/**/*.{js,jsx,ts,tsx}'
+
+    "int": "eslint 'src/**/*.{js,jsx,ts,tsx}'",
+    "lint:fix": "eslint --fix 'src/**/*.{js,jsx,ts,tsx}'",
+    "lint:conflict": "eslint --print-config .eslintrc.js | eslint-config-prettier-check",
+    "preinstall": "typesync”
+
